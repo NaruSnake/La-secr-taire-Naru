d0c8e476cbbc1554d0b623399aa9001a08491593
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-06-21 01:06:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const fs = require("fs");
+const token = process.env.TOKEN
+
+client.on("ready", () => {
+
+	var memberCount = client.users.size;
+	var serverCount = client.guilds.size;
+		client.user.setGame("bosse à la place de Naru");
+		client.user.setStatus("online");
+	console.log("Je suis Online");
+	console.log("Utilisateurs: " + memberCount + "\nServeurs: " + serverCount);
+});
+// dossier commands //
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    console.log
+    files.forEach(file => {
+        console.log
+        let eventFunction = require(`./commands/${file}`);
+        console.log
+        let eventName = file.split(".")[0];
+        console.log
+        client.on(eventName, (...args) => eventFunction.run(client, ...args));
+        console.log
+    });
+});
+
+
+client.on("message", (message) => {
+    console.log
+    if (message.author.bot) return;
+    console.log
+    if (message.content.indexOf(config.prefix) !==0) return;
+    console.log
+    
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    // dossier commands//
+    try { 
+        console.log
+        let commandFile = require(`./commands/${command}.js`);
+        console.log
+        commandFile.run(client, message, args);
+        console.log
+
+        } catch (err) {
+            console.error(err);
+            console.log
+        }
+
+    });
+
+
+
+client.login(token);
