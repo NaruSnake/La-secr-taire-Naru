5451e00ddb7f192f68810d2fc55aa7e9fe3e773b
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-09 16:58:23 +0200

    Add files via upload

---
+++ commands/bg&e.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00FE00)
+    .setDescription("Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Beyond Good & Evil entre 20h30 - 21h")
+    .setImage("https://i.pinimg.com/564x/1e/b3/f4/1eb3f45c190d12eee9e692c8b964e03f.jpg")
+    message.delete();
+    message.channel.send({embed}).catch(console.error);
+    console.log
+}
