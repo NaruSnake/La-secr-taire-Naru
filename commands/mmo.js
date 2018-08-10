0b6de33c7ed1f2ef60128cb2d9ff864807e769ba
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-10 13:42:24 +0200

    Add files via upload

--- commands/mmo.js
+++ commands/mmo.js
@@ -2,8 +2,8 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
-    .setDescription("Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Beyond Good & Evil entre 20h30 - 21h")
-    .setImage("https://i.pinimg.com/564x/1e/b3/f4/1eb3f45c190d12eee9e692c8b964e03f.jpg")
+    .setDescription("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Révélation Online entre 20h30 - 21h")
+    .setImage("https://i.pinimg.com/originals/6d/e8/b7/6de8b7f16890eb2268e925bb2125afdc.png")
     message.delete();
     message.channel.send({embed}).catch(console.error);
     console.log
