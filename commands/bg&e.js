9bf3d1d79315279a2b754b09f5762533afcc728d
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-11 12:45:29 +0200

    Add files via upload

--- commands/bg&e.js
+++ commands/bg&e.js
@@ -2,9 +2,9 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
-    .setDescription("Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Beyond Good & Evil entre 20h30 - 21h")
     .setImage("https://i.pinimg.com/564x/1e/b3/f4/1eb3f45c190d12eee9e692c8b964e03f.jpg")
     message.delete();
+    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Beyond Good & Evil entre 20h30 - 21h").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
--- commands/moemon.js
+++ commands/moemon.js
@@ -2,9 +2,9 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
-    .setDescription("@everyone Retrouvez NaruSnake pour faire du Moemon Fire Red Random, le random à été faite par le DrLinkInk & co ce soir vers 20h30 - 21h ")
     .setImage("https://i.pinimg.com/originals/a3/26/8b/a3268b939fff646d0f7441f6af7974b6.png")
     message.delete();
+    message.channel.send("@everyone Retrouvez NaruSnake pour faire du Moemon Fire Red Random, le random à été faite par le DrLinkInk & co ce soir vers 20h30 - 21h").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
