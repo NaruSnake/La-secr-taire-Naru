76d82c9ac14b3189512f8b883cdd16ec2156c864
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-11 12:34:58 +0200

    Add files via upload

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00FE00)
+    .setImage("https://i.pinimg.com/originals/6d/e8/b7/6de8b7f16890eb2268e925bb2125afdc.png")
+    message.delete();
+    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Révélation Online entre 20h30 - 21h").catch(console.error);
+    message.channel.send({embed}).catch(console.error);
+    console.log
+}
