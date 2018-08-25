68a34260a2da335b97c6df3a7cd1c75fcbe401b4
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-25 17:10:02 +0200

    Add files via upload

--- commands/mmo.js
+++ commands/mmo.js
@@ -2,9 +2,9 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
-    .setImage("https://i.pinimg.com/originals/6d/e8/b7/6de8b7f16890eb2268e925bb2125afdc.png")
+    .setImage("https://i.pinimg.com/originals/56/3e/e1/563ee155181249f6410d5ba2bd393c11.png")
     message.delete();
-    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Révélation Online entre 20h30 - 21h").catch(console.error);
+    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Black Desert Online entre 20h30 - 21h").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
