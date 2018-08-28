7fccf8d4617a9419254d411640fcab053c631d80
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-28 12:43:52 +0200

    Add files via upload

--- commands/moemon.js
+++ commands/moemon.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     .setColor(0x00FE00)
     .setImage("https://i.pinimg.com/originals/a3/26/8b/a3268b939fff646d0f7441f6af7974b6.png")
     message.delete();
-    message.channel.send("@everyone Retrouvez NaruSnake pour faire du Moemon Fire Red Random, le random à été faite par le DrLinkInk & co ce soir vers 20h30 - 21h").catch(console.error);
+    message.channel.send("@everyone Retrouvez NaruSnake pour faire du Moemon Fire Red Random, le random à été faite par le DrLinkInk & co ce soir à 14h").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
