4082b44d2d385c154a3cb6c6219dadaefcdba687
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-08-31 18:18:55 +0200

    Add files via upload

--- commands/bdo.js
+++ commands/bdo.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     .setColor(0x00FE00)
     .setImage("https://i.pinimg.com/originals/56/3e/e1/563ee155181249f6410d5ba2bd393c11.png")
     message.delete();
-    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Black Desert Online entre 20h30 - 21h sur www.twitch.tv/narusnake").catch(console.error);
+    message.channel.send("@everyone Retrouvez NaruSnake en compagnie de AdryNeko ce soir sur Black Desert Online entre 20h30 - 21h sur <B>www.twitch.tv/narusnake </B> ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
