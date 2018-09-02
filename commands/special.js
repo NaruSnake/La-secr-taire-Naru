ba40633e32c3f0dafb2b6a0c8d43d8d880ed6c8b
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-02 15:41:47 +0200

    Add files via upload

--- commands/special.js
+++ commands/special.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
     .setImage("https://i.pinimg.com/originals/b9/b8/68/b9b868075ea0b5f268103cbbedb87b46.png")
     message.delete();
-    message.channel.send("@everyone Rendez-vous ce soir pour une soirée spécial rentrée scolaire en compagnie de NaruSNake sur 2 jeux sur le theme de l'école comme d'habitude on se retrouve vers 20h30 - 21h").catch(console.error);
+    message.channel.send("@everyone Rendez-vous ce soir pour une soirée spéciale rentrée scolaire en compagnie de NaruSNake sur 2 jeux sur le thème de l'école comme d'habitude on se retrouve vers 20h30 - 21h").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
