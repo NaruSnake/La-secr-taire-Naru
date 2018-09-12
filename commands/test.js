420611e1c285b7800030e33a56210e6b4c66f0b4
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-12 21:14:34 +0200

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,9 @@
+//commande say//
+
 exports.run = (client, message, args) => {
-    const Discord = require('discord.js');
-    const embed = new Discord.RichEmbed()
-    .setColor(0x00FE00)
-    .setImage("")
+    let text = args.slice(0).join(" ");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("ta pas le droit connard")
     message.delete();
-    message.channel.send("@everyone").catch(console.error);
-    message.channel.send({embed}).catch(console.error);
+    message.channel.send(text).catch(console.error);
     console.log
 }
