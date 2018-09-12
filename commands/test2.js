03d078d482af0edd462d76695e096b280e0e013d
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-12 22:10:36 +0200

    Add files via upload

--- commands/test2.js
+++ commands/test2.js
@@ -3,7 +3,7 @@
 exports.run = (client, message, args) => {
     let text = args.slice(0).join(" ");
    if (message.delete(),
-    message.memeber.roles(Testeurs_Bots),
+    message.member.roles(Testeurs_Bots),
     message.channel.send(text).catch(console.error));
    if (message.delete(),
    message.channel.send("on ne joue pas avec moi").catch(console.error));
