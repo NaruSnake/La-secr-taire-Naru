dbf9d2050fd93bfcbca070d9e893801f575abc69
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-12 21:53:46 +0200

    Add files via upload

--- commands/test2.js
+++ commands/test2.js
@@ -2,9 +2,10 @@
 
 exports.run = (client, message, args) => {
     let text = args.slice(0).join(" ");
-   if ( message.delete())
-        message.member.hasPermission("MANAGE_MESSAGES"); return message.channel.send("On ne joue pas avec moi");
-  let   ( message.delete())
-     ( message.channel.send(text).catch(console.error))
+   if (message.delete());
+    message.member.hasPermission("MANAGE_MESSAGES"); 
+    message.channel.send(text).catch(console.error);
+   if (message.delete());
+   message.channel.send("on ne joue pas avec moi").catch(console.error);
     console.log
 }
