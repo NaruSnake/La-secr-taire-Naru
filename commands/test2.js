26df061970934606605a47407aef35d924384cbe
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-12 23:00:25 +0200

    Add files via upload

--- commands/test2.js
+++ commands/test2.js
@@ -2,11 +2,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-var Testeurs_Bots
+
 
 exports.run = (client, message, args) => {
     let text = args.slice(0).join(" ");
-  if  (message.member.roles(Testeurs_Bots),
+  iif (!message.member.roles.some(r=>["role_name"].includes(r.name)) ) 
+  return;
         message.delete(),
          message.channel.send(text).catch(console.error));
    if (message.delete(),
