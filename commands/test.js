278b85804f428e0dfb60a6b95ad501bb12ffa159
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-09-12 21:25:28 +0200

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@
 
 exports.run = (client, message, args) => {
     let text = args.slice(0).join(" ");
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("tu as pas le droit connard")
+    message.member.hasPermission("MANAGE_MESSAGES"); return message.channel.send("tu as pas le droit connard");
     message.delete();
     message.channel.send(text).catch(console.error);
     console.log
