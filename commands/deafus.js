b8992f148aa09050a1558690ff43454beebbccc3
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-11-01 20:40:48 +0100

    Add files via upload

--- commands/deafus.js
+++ commands/deafus.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     .setColor(0x00FE00)
     .setImage("https://i.pinimg.com/originals/48/96/08/489608a6e4b88bbe6f9ab17c7ab177b4.gif")
     message.delete();
-    message.channel.send("@everyone Retrouvez ce soir Naru et Deafus pour faire le ménage de l'extrême dans la joie et la bonne humeur...").catch(console.error);
+    message.channel.send("@everyone Retrouvez ce soir Naru et Deafus pour faire le ménage de l'extrême dans la joie et la bonne humeur").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
