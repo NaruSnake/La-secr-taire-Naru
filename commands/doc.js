8025c66829dbb3ae38424a06dfd43e75d5608e18
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-11-04 12:35:42 +0100

    Add files via upload

--- commands/doc.js
+++ commands/doc.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
     .setImage("https://i.pinimg.com/originals/b6/2a/99/b62a993087458aed0f68eef6c1c38934.png")
     message.delete();
-    message.channel.send("@everyone Ce soir vers 20h retrouvez le RetroStreamInk du Dr Link Ink sur Youtube et Twitch !").catch(console.error);
+    message.channel.send("@everyone Ce soir vers 20h retrouvez le live du Retro StreamInk avec le Dr Link Ink sur Youtube et Twitch. \n Pendant ce live il fera gagner un petit lot à deux d'entre vous ! Soyez là :smiley:").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
