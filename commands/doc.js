541cf5c1cce394d7fd4e52906144661405affce2
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2018-12-16 15:03:58 +0100

    Update doc.js

--- commands/doc.js
+++ commands/doc.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
-    .setImage("https://i.pinimg.com/originals/02/19/f9/0219f95c6eaa579ae092d2d72e3f4d42.jpg")
+    .setImage("https://i.pinimg.com/originals/ba/30/29/ba3029650bc3844860caa03b8a841773.jpg")
     message.delete();
     message.channel.send("@everyone Ce soir vers 20h retrouvez le live du Retro StreamInk avec le Dr Link Ink sur Youtube et Twitch.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
