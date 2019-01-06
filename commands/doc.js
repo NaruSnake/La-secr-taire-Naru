f738dd788016dd35419a1d1c33302ed642e3eba8
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-01-06 13:09:16 +0100

    Add files via upload

--- commands/doc.js
+++ commands/doc.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
-    .setImage("https://i.pinimg.com/originals/02/19/f9/0219f95c6eaa579ae092d2d72e3f4d42.jpg")
+    .setImage("https://i.pinimg.com/originals/f9/d8/6a/f9d86a4ea7b52f7c4460d39f20ad79da.jpg")
     message.delete();
     message.channel.send("@everyone Ce soir vers 20h retrouvez le live du Retro StreamInk avec le Dr Link Ink sur Youtube et Twitch.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
