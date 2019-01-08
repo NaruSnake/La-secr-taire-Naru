118e3ee930951b6e781d55110cf35fd9eacccdc9
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-01-08 17:54:40 +0100

    Add files via upload

--- commands/aniv.js
+++ commands/aniv.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     .setColor(0x00FE00)
     .setImage("https://i.pinimg.com/originals/78/65/e9/7865e9137e3aa105d0b32de417be30cb.gif")
     message.delete();
-    message.channel.send("Bonne anniversaire à @Deafus#4831 et @Lawliet53#7778 même si c'est pas aujourd'hui car l'autre con d'admin sais plus si c'était aujourd'hui ou demain donc on préfère lui souhaiter aujourd'hui on leur ramène tous un gâteau et on leur fait tous des bisoux ").catch(console.error);
+    message.channel.send("Bonne anniversaire à Petite Croquette on leur ramène tous un gâteau et on leur fait tous des bisoux ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
