36807039d0b8f6e4da907f38ce660fff0c912b92
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-01-23 12:59:33 +0100

    Add files via upload

--- commands/doc2.js
+++ commands/doc2.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
     .setImage("https://i.pinimg.com/564x/02/81/15/028115a43612f576dce83b92677ccbd0.jpg")
     message.delete();
-    message.channel.send("@everyone  Retrouvez le Docteur Link Ink vers 14h pour un live Youtube et Twitch sur une hack-rom Pokémon : Ruby Oméga : Glowin Garnet.").catch(console.error);
+    message.channel.send("@everyone  Retrouvez le Docteur Link Ink aux alentours de 15h pour un live Youtube et Twitch sur une hack-rom Pokémon : Ruby Oméga : Glowin Garnet.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
