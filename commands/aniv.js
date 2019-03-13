4fa626020ae0464a8c76c3136ea008b27a695730
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-03-13 22:20:39 +0100

    Add files via upload

--- commands/aniv.js
+++ commands/aniv.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     .setColor(0x00FE00)
     .setImage("https://i.pinimg.com/originals/78/65/e9/7865e9137e3aa105d0b32de417be30cb.gif")
     message.delete();
-    message.channel.send("Bonne anniversaire à Saito on leur ramène tous un gâteau et on leur fait tous des bisoux ").catch(console.error);
+    message.channel.send("Bonne anniversaire à Zéras on leur ramène tous un gâteau et on leur fait tous des bisoux ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
