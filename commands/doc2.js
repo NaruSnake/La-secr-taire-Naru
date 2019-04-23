64d6e594d5c48629cfd518c646f689e2e3a044e8
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-04-23 23:06:12 +0200

    Add files via upload

--- commands/doc2.js
+++ commands/doc2.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
-    .setImage("https://i.pinimg.com/564x/02/81/15/028115a43612f576dce83b92677ccbd0.jpg")
+    .setImage("https://i.pinimg.com/originals/cd/f8/21/cdf8218ee8c0f15cd8ac2966fcd30fda.jpg")
     message.delete();
     message.channel.send("@everyone  Retrouvez le Docteur Link Ink aux alentours de 15h pour un live Youtube et Twitch sur une hack-rom Pokémon : Ruby Oméga : Glowin Garnet.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
