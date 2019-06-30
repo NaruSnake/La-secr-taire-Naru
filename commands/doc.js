b4adc8558402969a53029721d82b48de55f12752
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-06-30 13:50:44 +0200

    Add files via upload

--- commands/doc.js
+++ commands/doc.js
@@ -3,9 +3,9 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.youtube.com/channel/UCoCIHOxDx_RG6y-rVk71oFA")
-    .setImage("https://i.pinimg.com/originals/92/b8/bb/92b8bbe528dffb6b14aea7ced94fd6eb.jpg")
+    .setImage("https://i.pinimg.com/originals/99/f5/93/99f593f33474ebb5e4d3bb13303d1a9d.jpg")
     message.delete();
-    message.channel.send("@everyone Ce soir vers 20h retrouvez le RetroStreamInk du Dr Link Ink sur Youtube et Twitch !").catch(console.error);
+    message.channel.send("@everyone Ce soir vers 20h retrouvez le live du Retro StreamInk avec le Dr Link Ink sur Youtube et Twitch.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
--- commands/srre1.js
+++ commands/srre1.js
@@ -2,10 +2,10 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
-    .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
+    .setDescription(" Le lien de la chaîne: https://www.twitch.tv/teraeurope")
     .setImage("https://i.pinimg.com/originals/9f/2b/3b/9f2b3bcccead8aade83dabd2669f99c7.png")
     message.delete();
-    message.channel.send("@everyone Retrouvez NaruSnake pour un peu de Speedrun sur Resident Evil et peut etre autre chose si le jeu ne l'aime pas entre 20h30 - 21h").catch(console.error);
+    message.channel.send("@everyone TeraEurope lance un stream sur les nouveautés de la nouvelle mise à jour si ça vous interesse c'est ici que ça ce passe https://www.twitch.tv/teraeurope").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
