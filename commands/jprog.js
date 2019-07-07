ddc2837aad65b1bad85266f72da40cfc40f7f914
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-07-07 20:27:18 +0200

    Update jprog.js

--- commands/jprog.js
+++ commands/jprog.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
     .setImage("https://i.pinimg.com/originals/d0/83/eb/d083eb7efe43ecdd0b32340919bcb65f.png")
     message.delete();
-    message.channel.send("@everyone Voici le programme de la semaine en espérant qu'il vous plairat, des changement peuve etre amener notamment sur les jour de repos. Sans oublier que les JDR sont toujours la mais pas facile de les placé dans le programme donc resté conencté sur le discord je donne les infos au moment voulu. Des bisoux").catch(console.error);
+    message.channel.send("@everyone Voici le programme de la semaine en espérant qu'il vous plaira, des changements peuvent être amener notamment sur les jour de repos. Sans oublier que les JDR sont toujours la mais pas facile de les placé dans le programme donc resté connecté sur le discord je donne les infos au moment voulu. Des bisoux").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
