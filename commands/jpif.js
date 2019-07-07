334e2ac1f7c1cf339029d528095a8b625344b80c
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-07-07 20:21:56 +0200

    Add files via upload

--- commands/jpif.js
+++ commands/jpif.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
-    .setImage("https://i.pinimg.com/originals/ae/1d/f7/ae1df70967d535489d51e9f75fc53c58.png")
+    .setImage("https://i.pinimg.com/originals/c4/e1/6d/c4e16d4dc6919390a81b5bc3fa6b7f00.png")
     message.delete();
     message.channel.send("@everyone Ce soir c'est le jeux au pif on débute le stream vers 20h30").catch(console.error);
     message.channel.send({embed}).catch(console.error);
--- commands/jprog.js
+++ commands/jprog.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
-    .setImage("https://i.pinimg.com/originals/82/c7/7c/82c77c89fccc98a2fc8b983ad0ac5fab.png")
+    .setImage("https://i.pinimg.com/originals/d0/83/eb/d083eb7efe43ecdd0b32340919bcb65f.png")
     message.delete();
     message.channel.send("@everyone Voici le programme de la semaine en espérant qu'il vous plairat, des changement peuve etre amener notamment sur les jour de repos. Sans oublier que les JDR sont toujours la mais pas facile de les placé dans le programme donc resté conencté sur le discord je donne les infos au moment voulu. Des bisoux").catch(console.error);
     message.channel.send({embed}).catch(console.error);
