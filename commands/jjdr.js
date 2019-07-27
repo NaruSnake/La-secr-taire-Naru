c8601581fb39cc4f60e2f3e31f2e330638710a75
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-07-27 12:11:43 +0200

    Add files via upload

--- commands/jjdr.js
+++ commands/jjdr.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
     .setImage("https://i.pinimg.com/originals/c3/40/6d/c3406da99afa0e42de8e44f381ef8705.png")
     message.delete();
-    message.channel.send("@everyone Ce soir petit changement de programme pas de shantae mais du JDR sur Derniere Donne a 21h soyez la").catch(console.error);
+    message.channel.send("@everyone on se retrouve pour du JDR dans environs 1h soyez la").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
