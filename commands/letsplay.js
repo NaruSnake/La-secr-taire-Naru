77a34510de06dac2906d9b70d8ef6cd67f24600b
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-08-18 14:21:04 +0200

    Add files via upload

--- commands/letsplay.js
+++ commands/letsplay.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
     .setImage("https://i.pinimg.com/originals/9d/d9/11/9dd911620c251dc66b39d452b5448d3f.png")
     message.delete();
-    message.channel.send("@everyone Ce soir on va faire du let's play en mode pépére on débute le stream dans 1h environ").catch(console.error);
+    message.channel.send("@everyone Ce soir on va faire du let's play en mode pépère on débute le stream dans 1h environ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
     console.log
 }
