254ba15565b3fa57f0021a77a92c4cc5e459ddde
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-09-29 10:31:21 +0200

    Add files via upload

--- commands/random.js
+++ commands/random.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
-    .setImage("https://i.pinimg.com/originals/34/59/97/3459974ea7b0c3c5ed51f101bbf6f739.png")
+    .setImage("https://i.pinimg.com/originals/12/91/39/1291391aa8378d404596ec161e1a9f72.png")
     message.delete();
     message.channel.send("@everyone Ce soir on va faire du Randomizer, Naru va t'il etre salé ? on débute le stream dans 1h environ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
