4eae8a0e58c3956fc73668ea99c7a68011bad97c
NaruSnake <35462738+NaruSnake@users.noreply.github.com>
2019-10-06 11:02:11 +0200

    Add files via upload

--- commands/letsplay.js
+++ commands/letsplay.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
-    .setImage("https://i.pinimg.com/originals/54/11/53/54115315194bc18849c5635e1da992d0.png")
+    .setImage("https://i.pinimg.com/originals/b9/84/b2/b984b273d37148e2767c28a349767275.png")
     message.delete();
     message.channel.send("@everyone Ce soir on va faire du let's play en mode pépère on débute le stream dans 1h environ").catch(console.error);
     message.channel.send({embed}).catch(console.error);
--- commands/prog.js
+++ commands/prog.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setColor(0x00FE00)
     .setDescription(" Le lien de la chaîne: https://www.twitch.tv/narusnake")
-    .setImage("https://i.pinimg.com/originals/3e/c5/7b/3ec57bc9f6b4bad29eb5150f7006ebb4.png")
+    .setImage("https://i.pinimg.com/originals/00/5d/27/005d2775c19c6d3dd5d866af0b7fe0d0.png")
     message.delete();
     message.channel.send("@everyone Voici ce qu'il y a au programme en ce moment, des annonces seront faites tous les jours pour vous informer de ce que l'on va faire 1h avant le stream à peu près. Je posterais ce message à chaque changement dans le programme. Soyez-là pour ne rien louper.").catch(console.error);
     message.channel.send({embed}).catch(console.error);
